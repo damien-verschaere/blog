803c1b2652e8b6d56823d3b6803002ca8f2900f0
Johan <johan.bouguermouh@laplateforme.io>
2021-12-13 15:38:51 +0100

    ajouter les élément contextuel pour CSS

--- functions/function.php
+++ functions/function.php
@@ -5,6 +5,22 @@
         mysqli_set_charset($connexion, 'utf8');
         return $connexion;
     }
+    /*------------------- Donner les droit pour l'affichage------------------- */
+    function droit_user(){
+        if(!isset($_SESSION['id_droits'])){
+            $droit_user = 'none';
+        }
+        elseif($_SESSION['id_droits']=='42'){
+            $droit_user = 'modo';
+        }
+        elseif($_SESSION['id_droits']=='1337'){
+            $droit_user = 'admin';
+        }
+        elseif($_SESSION['id_droits']=='1'){
+            $droit_user = 'user';
+        }
+        return  $droit_user;
+    }
     /*----------------------------PAGE INSCRIPTION------------------------------- */
     function inscription(){
         $message='';
--- index.php
+++ index.php
@@ -1,27 +1,4 @@
 <?php
 session_start();
-require('functions/function.php');
+header('location:./views/accueil.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        
-    </header>
-    <main>
-        <div class=com>
-        <?php affiche_article() ?>
-        </div>
-    </main>
-    <footer>
-
-    </footer>
-</body>
-</html>
