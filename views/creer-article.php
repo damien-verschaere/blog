973915cbf56fbe0cea0cbc5abfcd666b444647a2
damien-verschaere <damien.verschaere@laplateforme.io>
2021-12-14 12:41:47 +0100

    upload image + lien en bdd ok

--- views/creer-article.php
+++ views/creer-article.php
@@ -7,20 +7,16 @@ require "../functions/function.php";
 
 ?>
 <main>
-    <form action="creer-article.php"method="POST" >
+    <form action="creer-article.php"method="POST" enctype="multipart/form-data">
         <select name="categorie" >
         <?php affiche_categorie() ?>
         </select>
         <input type="text" name="titre" placeholder="titre article">
         <input type="text" name="description" placeholder="introduction article">
+        <input type="file" name="image_article" id="image_article">
         <textarea name="article" cols="30" rows="10"></textarea>
         <input type="submit" name="creer">
         <?php creer_article()?>
-    </form>
-    <form action="creer-article.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="image_article" id="image_article">
-        <input type="submit" name="send_image">
-        <?php image() ?>
     </form>
 </main>
 <?php
