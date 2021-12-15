6deec2377b0ff2f4d26433209831a7503ff61f18
max-machin <machin.max@lapalteforme.io>
2021-12-15 15:18:32 +0100

    remise en forme de l'affichage de la page inscription et début de l'affichage des commentaires de la page article

--- views/creer-article.php
+++ views/creer-article.php
@@ -1,24 +1,41 @@
 <?php
 session_start();
-echo $_SESSION['id'];
-require "requires/require_Header.php";
+
 require "../functions/function.php";
 
-
 ?>
-<main>
-    <form action="creer-article.php"method="POST">
-        <select name="categorie" >
-        <?php affiche_categorie() ?>
-        </select>
-        <input type="text" name="titre" placeholder="titre article">
-        <input type="text" name="description" placeholder="introduction article">
-        <textarea name="article" cols="30" rows="10"></textarea>
-        <input type="submit" name="creer">
-        <?php creer_article()?>
-    </form>
-</main>
-<?php
 
-require "requires/require_Footer.php";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require "requires/require_meta.php" ?>
+    <title>Créer-article</title>
+</head>
+
+<body>
+    <header>
+        <?php require "requires/require_Header.php" ?>
+    </header>
+    <main class="main_center">
+        <section class="section_creer_article">
+            <div class="display_form_creer_article">
+            <form action="creer-article.php" method="POST" enctype="multipart/form-data">
+                <select name="categorie">
+                    <?php affiche_categorie() ?>
+                </select>
+                <input type="text" name="titre" placeholder="titre article">
+                <input type="text" name="description" placeholder="introduction article">
+                <input type="file" name="image_article">
+                <textarea name="article" cols="30" rows="10"></textarea>
+                <input type="submit" name="creer">
+                <?php creer_article() ?>
+            </form>
+            </div>
+        </section>
+    </main>
+    <?php
+
+    require "requires/require_Footer.php";
+    ?>
+</body>
