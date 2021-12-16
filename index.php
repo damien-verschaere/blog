32eefb181d22a2617adc90fe72c7c1cd57ebd0c5
Johan <johan.bouguermouh@laplateforme.io>
2021-12-16 09:56:48 +0100

    ajout de la vidéo de bienvenue

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
 session_start();
-header('location:./views/accueil.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght400&family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="assets\css\style.css">
+    <link rel="icon" type="image/moodule_favicon.png" href="" />
+    <title>Beblog</title>
+</head>
+<body>
+    <video id="video_background_connect" playsinline autoplay muted src="assets\img\beblog_animation_entree.mp4" alt="vidéo bienvenue">
+</body>
+</html>
+<?php
+header("Refresh: 3.7;url=http://localhost/prod_blog/views/accueil.php");
 ?>
